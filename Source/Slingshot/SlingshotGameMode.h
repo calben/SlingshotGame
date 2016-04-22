52585562c1e952a5aafe80819ccaae82fc4b6360
Calem Bendell <calem.j.bendell@gmail.com>
2016-04-22 14:50:00 -0400

    add initial codebase

---
+++ Source/Slingshot/SlingshotGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "SlingshotGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SLINGSHOT_API ASlingshotGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
